71d64b379b85d4590fefdaef57e0cc63f568b2fe
dyk000825 <kdy000825@naver.com>
2025-02-03 15:32:17 +0900

    feat. fix: mara class fix

--- src/domain/Mara.java
+++ src/domain/Mara.java
@@ -1,14 +1,11 @@
 package domain;
-
-import domain.FoodItem;
-
 import java.util.ArrayList;
 
 public class Mara extends FoodItem {
     ArrayList<String> ingredients;
     Spiciness spiciness;
 
-    Mara(String name, int spicinessLevel) {
+    public Mara(String name, int spicinessLevel) {
         super(name);
         ingredients = new ArrayList<>();
         spiciness = new Spiciness(spicinessLevel);  // 맵기 설정
@@ -19,7 +16,7 @@ public class Mara extends FoodItem {
     }
 
     @Override
-    void display() {
+    public void display() {
         super.display();
         System.out.println("선택한 재료: " + ingredients);
         spiciness.displaySpiciness();  // 맵기 출력
